1cbfb7037ff5b7aef1e2ae133ca2e50f00425edb
Kha Le & Cory Ribson <student@epicodus.com>
2015-04-21 16:38:02 -0700

    Final for Scrabble

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -12,9 +12,18 @@
 
 require('rspec')
 require('scrabble')
+require('pry')
 
 describe('String#scrabble') do
-  it("will that the word is 2 or more letters") do
-    expect("Word".scrabble()).to(eq(true))
+  # it("turns all letters to uppercase") do
+  #   expect("Hello".scrabble()).to(eq("HELLO"))
+  # end
+  #
+  # it("check that the word is 2 or more letters") do
+  #   expect("Word".scrabble()).to(eq(true))
+  # end
+
+  it("Adds the total value for the word") do
+    expect("Hello".scrabble()).to(eq(8))
   end
 end
